2882743f36024fe5275a8f743169591546d19d29
Jan Redmann <janredmann1@gmail.com>
2021-08-19 10:59:34 +0200

    add testing intro

--- testing/src/fizzbuzz.js
+++ testing/src/fizzbuzz.js
@@ -1,1 +1,20 @@
 //
+function fizzBuzz(num, string = '') {
+	// if (num % 15 === 0) {
+	// 	return 'fizzbuzz';
+	// }
+	// if (num % 3 === 0) {
+	// 	return 'fizz';
+	// }
+	// if (num % 5 === 0) {
+	// 	return 'buzz';
+	// }
+	// return num;
+	if (num % 3 === 0) string += 'fizz';
+	if (num % 5 === 0) string += 'buzz';
+	return string || num;
+}
+
+if (typeof module !== undefined) {
+	module.exports = fizzBuzz;
+}
